ce64c321c2f4e048bf021de85d10c5ef36da1081
nvsavant <nvishwas@seas.upenn.edu>
2018-02-19 15:23:29 -0500

    BST Updates

--- src/bst/App.java
+++ src/bst/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		binarySearchTree bst = new binarySearchTree();
-		
+
 		bst.add(4);
 		bst.add(2);
 		bst.add(6);
@@ -12,10 +12,28 @@ public class App {
 		bst.add(3);
 		bst.add(5);
 		bst.add(7);
+		bst.add(0);
+		bst.add(100);
+
+		// bst.inOrderTraversal();
+		// bst.preOrderTraversal();
+		// bst.postOrderTraversal();
+
+		// bst.breadthFirstTraversal();
+
+//		System.out.println(bst.treeDepth());
 		
-		bst.inOrderTraversal();
-		bst.preOrderTraversal();
-		bst.postOrderTraversal();
+//		System.out.println(bst.numberOfNodes());
 		
+//		System.out.println(bst.numberOfLeafNodes());
+		
+//		System.out.println("Maximum:"+bst.max());
+//		System.out.println("Minimum:"+bst.min());
+		
+		
+//		System.out.println(bst.numberOfFullNodes());
+		System.out.println(bst.isBST());
+		
+
 	}
 }
--- src/bst/binarySearchTree.java
+++ src/bst/binarySearchTree.java
@@ -1,10 +1,15 @@
 package bst;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class binarySearchTree {
 	private Node root;
+	private Node prev;
 
 	public binarySearchTree() {
 		this.root = null;
+		this.setPrev(null);
 	}
 
 	public void add(int data) {
@@ -82,6 +87,157 @@ public class binarySearchTree {
 		postOrderTraversalNode(node.getLeftChild());
 		postOrderTraversalNode(node.getRightChild());
 		System.out.print(node.getData() + " ");
+	}
+
+	public void breadthFirstTraversal() {
+		if (this.root == null) {
+			System.out.println("BST is empty");
+			return;
+		}
+
+		Queue<Node> queue = new LinkedList<Node>();
+		Node tempNode = this.root;
+
+		while (tempNode != null) {
+			System.out.print(tempNode.getData() + " ");
+			if (tempNode.getLeftChild() != null) {
+				queue.add(tempNode.getLeftChild());
+			}
+
+			if (tempNode.getRightChild() != null) {
+				queue.add(tempNode.getRightChild());
+			}
+
+			tempNode = queue.poll();
+		}
+		System.out.println();
+	}
+
+	public int treeDepth() {
+		if (this.root == null) {
+			System.out.println("BST is empty");
+			return 0;
+		}
+		return treeDepthNode(this.root);
+	}
+
+	private int treeDepthNode(Node node) {
+		if (node == null) {
+			return 0;
+		}
+		int leftCount = treeDepthNode(node.getLeftChild());
+		int rightCount = treeDepthNode(node.getRightChild());
+
+		return (int) Math.max(leftCount, rightCount) + 1;
+	}
+
+	public int numberOfNodes() {
+		if (this.root == null) {
+			return 0;
+		}
+		return numberOfNodesNode(this.root);
+	}
+
+	private int numberOfNodesNode(Node node) {
+		if (node == null) {
+			return 0;
+		}
+		int leftCount = numberOfNodesNode(node.getLeftChild());
+		int rightCount = numberOfNodesNode(node.getRightChild());
+		return leftCount + rightCount + 1;
+	}
+
+	public int max() {
+		if (this.root == null) {
+			return -1;
+		}
+		return maxNode(this.root);
+	}
+
+	private int maxNode(Node node) {
+		if (node.getRightChild() == null) {
+			return node.getData();
+		}
+		return maxNode(node.getRightChild());
+	}
+
+	public int min() {
+		if (this.root == null) {
+			return -1;
+		}
+		return minNode(this.root);
+	}
+
+	private int minNode(Node node) {
+		if (node.getLeftChild() == null) {
+			return node.getData();
+		}
+		return minNode(node.getLeftChild());
+	}
+
+	public int numberOfFullNodes() {
+		if (this.root == null) {
+			return 0;
+		}
+		return numberOfFullNodesNode(this.root);
+	}
+
+	private int numberOfFullNodesNode(Node node) {
+		if (node.getLeftChild() == null || node.getRightChild() == null) {
+			return 0;
+		} else {
+			int leftCount = numberOfNodesNode(node.getLeftChild());
+			int rightCount = numberOfNodesNode(node.getRightChild());
+			return leftCount + rightCount;
+		}
+
+	}
+
+	public int numberOfLeafNodes() {
+		if (this.root == null) {
+			return 0;
+		}
+		return numberOfLeafNodesNode(this.root);
+	}
+
+	private int numberOfLeafNodesNode(Node node) {
+		if (node.getLeftChild() == null && node.getRightChild() == null) {
+			return 1;
+		} else {
+			return numberOfLeafNodesNode(node.getLeftChild()) + numberOfLeafNodesNode(node.getRightChild());
+		}
+
+	}
+
+	public boolean isBST() {
+		this.prev = null;
+		if (isBSTUtil(this.root) == false) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	private boolean isBSTUtil(Node node) {
+		if(node != null) {
+			if(!isBSTUtil(node.getLeftChild()))
+				return false;
+			
+			if(this.prev!=null && this.prev.getData()>=node.getData()) {
+				return false;
+			}
+			prev = node;
+			return isBSTUtil(node.getRightChild());
+		}
+		return true;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
 	}
 
 }
